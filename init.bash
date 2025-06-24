396a404b3188a1de4fb5b1f49f4cdb82c7097d65
nickstewart <nick.stewart1993@gmail.com>
2025-06-24 21:18:23 +0100

    initial commit

---
+++ init.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install zsh
+brew install ghostty
+brew install nvim
+brew install fzf
+brew install ripgrep
+brew install tmux
+brew install zoxide
+brew install eza
+brew install yazi
+
+brew install fastfetch
+mkdir ~/.config/fastfetch
+
+fastfetch -c ~/.config/fastfetch/config.jsonc
+echo 'source ~/.config/.zshrc' >> ~/.zprofile
+git config --global diff.tool nvimdiff
+
+tmux -f ~/.config/.tmux.conf
+git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+
+mkdir -p ~./tmux/plugins/catppuccin
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
--- nvim/init.lua
+++ nvim/init.lua
@@ -3,8 +3,8 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
-vim.cmd("set relativenumber")
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -23,56 +23,4 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "catppuccin-frappe" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
-
-vim.cmd.colorscheme "catppuccin-frappe"
-
-local builtin =require("telescope.builtin")
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-    ensure_installed = { "sql", "java", "javascript", "bash", "lua", "go", "properties", "terraform", "typescript", "yaml", "xml", "markdown_inline", "json", "jq", "javadoc", "html", "http" },
-    sync_install = false, 
-    highlight = { enable = true },
-    indent = { enable = true }
-})
-
-local user_grp = vim.api.nvim_create_augroup("LazyUserGroup", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "lazy",
-  desc = "Quit lazy with <esc>",
-  callback = function()
-    vim.keymap.set(
-      "n",
-      "<esc>",
-      function() vim.api.nvim_win_close(0, false) end,
-      { buffer = true, nowait = true }
-    )
-  end,
-  group = user_grp,
-})
+require("lazy").setup("plugins")
